cc60beae7a065c314276575cb0e1edcd4d378b24
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2021-10-22 21:04:22 -0400

    City Search Final

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from "react"; // this helps import components
+import "./App.css"; // this imports css so that the js file can use it
+
+// In this project we will allow the user to provide us a City name
+// and we will display all of the associated zip codes to the user.
 
 function City({ data }) {
+	// data is being passed to a component, when it is destructed
 	return (
 		<div>
-			City Name: {data.City}
+			Zip Code: {data.Zipcode}
 			<br />
 			State: {data.State}
 			<ul>
@@ -13,58 +17,66 @@ function City({ data }) {
 		</div>
 	);
 }
-// onChange checks the users change every single time
-function ZipSearchField(props) {
+
+function CitySearchField(props) {
+	//prop is being passed to a component
+
 	return (
 		<div>
-			Zip Code:
+			City Name :
 			<input type="text" onChange={props.changeHandler} />
+			{/* pass this change handler as a prop to component */}
 		</div>
 	);
 }
 
 class App extends Component {
+	//Use of ES6 Class that extends, also the (parent) component
 	state = {
-		zipCode: "",
-		cities: [],
+		cityName: "", //to store City Name
+		zipcode: [], // to store x amount of cities
 	};
-	// by using a arrow function you don't need to define a constructor
-	//promise is doing work asynchronosly which means it give it later
-	zipChanged = (event) => {
-		console.log(event.target.value);
-		this.setState({ zipCode: event.target.value });
-		// console.log(this.state.zipCode);
-
-		// fetches data from api
-		if (event.target.value.length === 5) {
-			fetch("http://ctp-zip-api.herokuapp.com/zip/" + event.target.value)
-				.then((res) => res.json())
+	CityChanged = (event) => {
+		console.log(event.target.value); // console log the user input
+		this.setState({ cityName: event.target.value }); //sets the cityName value to user input doesn't happen right away
+		var letters = /^[A-Za-z]+$/;
+		if (event.target.value.match(letters)) {
+			// if value matches the pattern
+			fetch("http://ctp-zip-api.herokuapp.com/city/" + event.target.value) //the best way because it's there
+				//can also use fetch("http://ctp-zip-api.herokuapp.com/zip/"`${this.state.cityName}`) string literall
+				//^^^string literally is computed at a different rate then the fetch.
+				.then((res) => res.json()) // call that sets the state when it is valid
 				.then((data) => {
 					console.log(data);
-					this.setState({ cities: data });
+					this.setState({ zipcode: data }); //save the data to zipcode
 				})
 				.catch((err) => {
 					console.log("No Results");
-					this.setState({ cities: [] });
+					this.setState({ zipcode: [] });
 				});
+			//if there is no data say no results in console, and sets state to empty.
 		}
 	};
 
 	render() {
+		//renders the state of change at DOM,returns JSX
 		return (
 			<div className="App">
+				{/* Creates a class name for App, enables css access */}
 				<div className="App-header">
-					<h2>Zip Code Search</h2>
+					{/* Creates a class name for the header of App, enables css access */}
+					<h2>City Name Search</h2>
 				</div>
-				<ZipSearchField
-					zipCode={this.state.zipCode}
-					changeHandler={this.zipChanged}
+				<CitySearchField //child component
+					cityName={this.state.cityName}
+					changeHandler={this.CityChanged}
+					// changeHandler is defined by CityChanged
 				/>
-				<div>Current Zip is {this.state.zipCode}</div>
+				<div>Current City is {this.state.cityName}</div>
+				{/* component that is for the input of search field */}
 				<div>
-					{this.state.cities.length === 0 ? <h1>No Result</h1> : null}
-					{this.state.cities.map((city) => (
-						<City data={city} />
+					{this.state.zipcode.map((zipC) => (
+						<p>{zipC}</p>
 					))}
 				</div>
 			</div>
@@ -74,5 +86,4 @@ class App extends Component {
 
 export default App;
 
-//cd zip-code-react-lab
-// cd  zip-search
+// cd  city-search
